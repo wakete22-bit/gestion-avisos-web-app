972c82d4a7f2a61c7e9a33c538d249c03594a6bd
Adrian <ashtoon95@gmail.com>
2025-06-16 23:54:02 +0200

    Actualización de dependencias de Angular a versiones más recientes y mejora del diseño de la página de inicio. Se añadieron nuevos componentes para mostrar un resumen de avisos y presupuestos, así como estilos personalizados para mejorar la presentación. También se importó la fuente Poppins para un mejor estilo tipográfico.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,6 +14,7 @@ export const routes: Routes = [
         redirectTo: 'home',
         pathMatch: 'full',
       }
+      
     ]
   }
 ];
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+import { gridOutline, notificationsOutline, timeOutline, cubeOutline, documentTextOutline, peopleOutline, settingsOutline, personCircleOutline, receipt, hourglass, alertCircleOutline, hourglassOutline, warningOutline, cashOutline, warning, document, alertCircle  } from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+
+addIcons({
+  'grid-outline': gridOutline,
+  'notifications-outline': notificationsOutline,
+  'time-outline': timeOutline,
+  'cube-outline': cubeOutline,  
+  'document-text-outline': documentTextOutline,
+  'cash-outline': cashOutline,
+  'people-outline': peopleOutline,
+  'settings-outline': settingsOutline,
+  'person-circle-outline': personCircleOutline,
+  'receipt': receipt,
+  'hourglass-outline': hourglassOutline,
+  'alert-circle-outline': alertCircleOutline,
+  'warning': warning,
+  'document': document,
+  'alert-circle': alertCircle
+});
+
+export interface Aviso {
+  numero: string;
+  estado: string;
+  nombre: string;
+  detalle: string;
+  fecha: string;
+  urgente: boolean;
+  direccion: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, MatTableModule, MatIconModule],
 })
 export class HomePage {
+  displayedColumns: string[] = ['numero', 'estado', 'nombre', 'detalle', 'fecha', 'urgente', 'direccion', 'acciones'];
+  avisos: Aviso[] = [
+    {
+      numero: '001',
+      estado: 'En curso',
+      nombre: 'Restaurante El Sol',
+      detalle: 'Mantenimiento preventivo de 3 equipos A/C.',
+      fecha: '2025-05-27',
+      urgente: false,
+      direccion: 'Pza. de la Virgen 3'
+    },
+    // Puedes añadir más avisos aquí
+  ];
   constructor() {}
 }
