0c1a528b4d9ece20e91bd88d25f3fe8bf287cf18
Adrian <ashtoon95@gmail.com>
2025-06-17 17:24:23 +0200

    Se implementó la funcionalidad para abrir un modal al hacer clic en el botón "Añadir cliente" en la página de clientes. Se añadió el componente CrearClienteModalComponent y se ajustó la lógica para manejar la creación de nuevos clientes desde el modal. Además, se realizaron cambios en el HTML para integrar la nueva funcionalidad.

--- src/app/modules/clientes/pages/clientes/clientes.component.ts
+++ src/app/modules/clientes/pages/clientes/clientes.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonContent, IonIcon } from '@ionic/angular/standalone';
+import { IonContent, IonIcon, ModalController } from '@ionic/angular/standalone';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { addIcons } from 'ionicons';
 import { mapOutline,addCircle,alertCircle,close,eyeOutline,add,addCircleOutline } from 'ionicons/icons';
+import { CrearClienteModalComponent } from 'src/app/modules/avisos/components/crear-cliente-modal/crear-cliente-modal.component';
   
 
 interface Cliente {
@@ -42,10 +43,24 @@ export class ClientesComponent  implements OnInit {
     }
   ];
 
-  constructor() { 
+  constructor(private modalController: ModalController) { 
     addIcons({mapOutline,addCircle,alertCircle,close,eyeOutline,add,addCircleOutline});
   }
 
   ngOnInit() {}
+
+  async abrirModalCrearCliente() {
+    const modal = await this.modalController.create({
+      component: CrearClienteModalComponent,
+      cssClass: 'modal-crear-cliente',
+      showBackdrop: true,
+      backdropDismiss: true
+    });
+    await modal.present();
+    const { data, role } = await modal.onWillDismiss();
+    if (role === 'confirm' && data) {
+      console.log('Cliente creado:', data);
+    }
+  }
 
 }
